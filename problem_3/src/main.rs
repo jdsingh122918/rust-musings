4a6630018a1d33f954edf6b30fa1b08c584e426a
JD Singh <info@fermatsolutions.dev>
2023-08-17 15:37:49 -0500

    and uniqueness check for output
    Updated main function to include a timer to track execution time as well as an input for the large number to be factored. Also, added a new function to calculate unique values in the result. This update gives more robust and detailed result output and allows performance tracking.

--- problem_3/src/main.rs
+++ problem_3/src/main.rs
@@ -1,8 +1,14 @@
 /* Find the largest prime factor given an input
- */
+ */use std::time::{Instant};
+
 fn main() {
-    let result = calculate_prime_factors(100);
-    println!("{:?}", result);
+    let start = Instant::now();
+    let input = 600851475143;
+    let result = calculate_prime_factors(input);
+    let unique_result = calculate_unique_values(result);
+    let end = start.elapsed();
+    println!("Largest Prime Factor of {} is {}", input, unique_result[unique_result.len() - 1]);
+    println!("Execution Time: {}", end.as_millis());
 }
 
 fn calculate_prime_factors(mut input: i64) -> Vec<i32> {
@@ -26,4 +32,10 @@ fn calculate_prime_factors(mut input: i64) -> Vec<i32> {
         result.push(input as i32)
     }
     result
+}
+
+fn calculate_unique_values(mut input: Vec<i32>) -> Vec<i32> {
+    input.sort();
+    input.dedup();
+    input
 }
